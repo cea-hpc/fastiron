09e333edd9f0edc2f898681001aab9248cc7af5f
imrn99 <95699343+imrn99@users.noreply.github.com>
2023-02-23 15:54:09 +0100

    load_particle complete

--- src/mc/mc_utils.rs
+++ src/mc/mc_utils.rs
@@ -1,6 +1,6 @@
 use std::{cell::RefCell, rc::Rc};
 
-use num::Float;
+use num::{Float, zero};
 
 use crate::{montecarlo::MonteCarlo, particle_vault::ParticleVault};
 
@@ -10,11 +10,23 @@ use super::mc_particle::MCParticle;
 /// the active-particle data structure.
 pub fn load_particle<T: Float>(
     mcco: &MonteCarlo<T>,
-    mc_particle: &MCParticle<T>,
     particle_vault: &ParticleVault<T>,
     particle_idx: usize,
-) {
-    todo!()
+) -> MCParticle<T> {
+    let mut particle = particle_vault.get_particle(particle_idx).unwrap();
+
+    // update time to census
+    if particle.time_to_census <= zero() {
+        particle.time_to_census = particle.time_to_census + mcco.time_info.time_step;
+    }
+    // set age
+    if particle.age < zero() {
+        particle.age = zero();
+    }
+    // get energy group
+    particle.energy_group = mcco.nuclear_data.get_energy_groups(particle.kinetic_energy);
+
+    particle
 }
 
 /// Simulates the sources according to the problem's parameters.
