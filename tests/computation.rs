e7b781e45e13cc632732a36f395f84329d55ee8f
imrn99 <95699343+imrn99@users.noreply.github.com>
2023-03-01 09:22:59 +0100

    added test for sample_isotropic; added c files used to test original code

--- tests/computation.rs
+++ tests/computation.rs
@@ -4,7 +4,7 @@
 // to C++ functions and compare the results in the tests.
 // For now, printing will be just fine.
 
-use fastiron::mc::mc_rng_state::{spawn_rn_seed, pseudo_des};
+use fastiron::{mc::mc_rng_state::{spawn_rn_seed, pseudo_des}, direction_cosine::DirectionCosine};
 
 #[test]
 pub fn rng_spawned_number() {
@@ -15,11 +15,20 @@ pub fn rng_spawned_number() {
 }
 
 #[test]
-pub fn pseudo() {
+pub fn pseudo_hash() {
     let mut a: u32 = 123214124;
     let mut b: u32 = 968374242;
     pseudo_des(&mut a, &mut b);
     println!("a: {a}");
     println!("b: {b}");
     //panic!()
+}
+
+#[test]
+pub fn sample_isotropic() {
+    let mut dd: DirectionCosine<f64> = DirectionCosine { alpha: 0.2140, beta: 0.8621, gamma: 0.7821 };
+    let mut seed: u64 = 90374384094798327;
+    dd.sample_isotropic(&mut seed);
+    println!("dd: {dd:#?}");
+    panic!()
 }
---
+++ unit_test_c/main.cc
@@ -0,0 +1,41 @@
+#include "DirectionCosine.hh"
+#include "MC_RNG_State.hh"
+#include <stdint.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    printf("###########################\n");
+    printf("#   spawned number test   #\n");
+    printf("###########################\n");
+    uint64_t init_seed_test = 90374384094798327;
+    uint64_t* seed_test = &init_seed_test;
+    uint64_t res = rngSpawn_Random_Number_Seed(seed_test);
+    printf("spawned number: %lu\n", res);
+    
+    printf("\n");
+    printf("###########################\n");
+    printf("#     pseudo hash test    #\n");
+    printf("###########################\n");
+    uint32_t a = 123214124;
+    uint32_t b = 968374242;
+    pseudo_des(a, b);
+    printf("a: %u\n", a);
+    printf("b: %u\n", b);
+
+    printf("\n");
+    printf("###########################\n");
+    printf("#  sample isotropic test  #\n");
+    printf("###########################\n");
+    DirectionCosine dd = DirectionCosine(0.2140, 0.8621, 0.7821);
+    uint64_t init_seed = 90374384094798327;
+    uint64_t* seed = &init_seed;
+    dd.Sample_Isotropic(seed);
+    printf("alpha: %17.16f\n",  dd.alpha);
+    printf("beta: %17.16f\n" ,  dd.beta );
+    printf("gamma: %17.16f\n",  dd.gamma);
+
+    printf("\n");
+    printf("###########################\n");
+    printf("#  rotate 3d vector test  #\n");
+    printf("###########################\n");
+}
