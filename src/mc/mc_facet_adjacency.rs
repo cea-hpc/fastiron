e81e844845f3636a64c62e98cbd718964626f84a
imrn99 <95699343+imrn99@users.noreply.github.com>
2023-03-03 14:05:38 +0100

    completed mc_cell_state, mc_distance_to_facet, mc_facet_adjacency; updated structures to work around magic values

--- src/mc/mc_facet_adjacency.rs
+++ src/mc/mc_facet_adjacency.rs
@@ -1,10 +1,12 @@
 use super::mc_location::MCLocation;
 
+pub const N_POINTS_PER_FACET: usize = 3;
+
 /// Enum used to categorize the event a particle
 /// undergo when reaching a given facet.
 #[derive(Debug)]
 pub enum MCSubfacetAdjacencyEvent {
-    AdjacencyUndefined,
+    AdjacencyUndefined = 0,
     BoundaryEscape,
     BoundaryReflection,
     TransitOnProcessor,
@@ -17,14 +19,21 @@ pub struct SubfacetAdjacency {
     pub event: MCSubfacetAdjacencyEvent,
     pub current: MCLocation,
     pub adjacent: MCLocation,
-    pub neighbor_index: usize,
-    pub neighbor_global_domain: usize,
-    pub neighbor_foreman: usize,
+    pub neighbor_index: Option<usize>,
+    pub neighbor_global_domain: Option<usize>,
+    pub neighbor_foreman: Option<usize>,
 }
 
 impl Default for SubfacetAdjacency {
     fn default() -> Self {
-        todo!()
+        Self {
+            event: MCSubfacetAdjacencyEvent::AdjacencyUndefined,
+            current: Default::default(),
+            adjacent: Default::default(),
+            neighbor_index: None,
+            neighbor_global_domain: None,
+            neighbor_foreman: None,
+        }
     }
 }
 
@@ -32,25 +41,23 @@ impl Default for SubfacetAdjacency {
 #[derive(Debug)]
 pub struct MCFacetAdjacency {
     pub subfacet: SubfacetAdjacency,
-    pub num_points: usize,
-    pub point: [u32; 3],
+    pub num_points: usize, // may be able to delete later ?
+    pub point: [Option<usize>; N_POINTS_PER_FACET],
 }
 
 impl Default for MCFacetAdjacency {
     fn default() -> Self {
-        todo!()
+        Self {
+            subfacet: Default::default(),
+            num_points: N_POINTS_PER_FACET,
+            point: [None; N_POINTS_PER_FACET],
+        }
     }
 }
 
 /// Structure encompassing all adjacent facet to a cell.
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct MCFacetAdjacencyCell {
-    pub facet: Vec<MCFacetAdjacency>,
-    pub point: Vec<usize>,
-}
-
-impl Default for MCFacetAdjacencyCell {
-    fn default() -> Self {
-        todo!()
-    }
+    pub facet: [MCFacetAdjacency; 24], // need to find the correct way we get 24 to rewrite it with const
+    pub point: [usize; 14], // same with 14
 }
