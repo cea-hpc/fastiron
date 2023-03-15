4622c02d09dd31dc32f734da610e150cd9f83bc9
imrn99 <95699343+imrn99@users.noreply.github.com>
2023-03-15 11:33:23 +0100

    fixed a wrong computation in seg outcome; now census # is incoherent

--- src/cycle_tracking.rs
+++ src/cycle_tracking.rs
@@ -100,9 +100,10 @@ pub fn cycle_tracking_function<T: CustomFloat>(
                 let processed_vault =
                     &mut mcco.particle_vault_container.processed_vaults[processed_vault_idx];
 
+                // PARTICLE ARE INVALIDATED IN CYCLE TRACKING GUTS
                 // set the particle as processed, i.e. transfer it from processing to processed vault
-                processed_vault.push_particle(particle.clone());
-                processing_vault.invalidate_particle(particle_idx);
+                //processed_vault.push_particle(particle.clone());
+                //processing_vault.invalidate_particle(particle_idx);
 
                 // atomic in original code
                 mcco.tallies.balance_task[tally_idx].census += 1;
