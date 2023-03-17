93a3ec7388d7971254eefbf6402271ad49cbdc48
imrn99 <95699343+imrn99@users.noreply.github.com>
2023-03-17 11:09:38 +0100

    cleanup in mc_init

--- src/init_mc.rs
+++ src/init_mc.rs
@@ -247,10 +247,9 @@ fn init_mesh<T: CustomFloat>(mcco: &mut MonteCarlo<T>) {
 
     let mut comm: CommObject = CommObject::new(&partition);
     // indexing should be coherent since we cloned partition in comm's construction
-    partition
-        .iter_mut()
-        .enumerate()
-        .for_each(|(mesh_p_idx, mesh_p)| {
+    (0..comm.partition.len())
+        .into_iter()
+        .for_each(|mesh_p_idx| {
             let remote_cells =
                 comm.partition[mesh_p_idx].build_mesh_partition(&global_grid, &domain_centers);
 
