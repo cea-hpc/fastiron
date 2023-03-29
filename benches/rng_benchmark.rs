fcc3e781e74f4f166235438ae0a9869dc5cb67dc
imrn99 <95699343+imrn99@users.noreply.github.com>
2023-03-29 09:03:37 +0200

    added bench for other rand Rng types

--- benches/rng_benchmark.rs
+++ benches/rng_benchmark.rs
@@ -12,8 +12,24 @@ fn custom_sample(times: usize) {
     });
 }
 
-fn rand_sample(times: usize) {
+fn rand_std_sample(times: usize) {
     let mut rng: StdRng = rand::SeedableRng::seed_from_u64(9017476812930);
+    (0..times).for_each(|_| {
+        let res: f64 = rng.gen();
+        black_box(res);
+    });
+}
+
+fn rand_small_sample(times: usize) {
+    let mut rng: SmallRng = rand::SeedableRng::seed_from_u64(9017476812930);
+    (0..times).for_each(|_| {
+        let res: f64 = rng.gen();
+        black_box(res);
+    });
+}
+
+fn rand_thread_sample(times: usize) {
+    let mut rng: ThreadRng = rand::thread_rng();
     (0..times).for_each(|_| {
         let res: f64 = rng.gen();
         black_box(res);
@@ -35,9 +51,19 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         |b, &n| b.iter(|| custom_sample(n)),
     );
     group.bench_with_input(
-        BenchmarkId::new("rand crate RNG", "number of iterations"),
+        BenchmarkId::new("rand crate StdRng", "number of iterations"),
         &n_iter,
-        |b, &n| b.iter(|| rand_sample(n)),
+        |b, &n| b.iter(|| rand_std_sample(n)),
+    );
+    group.bench_with_input(
+        BenchmarkId::new("rand crate SmallRng", "number of iterations"),
+        &n_iter,
+        |b, &n| b.iter(|| rand_small_sample(n)),
+    );
+    group.bench_with_input(
+        BenchmarkId::new("rand crate ThreadRng", "number of iterations"),
+        &n_iter,
+        |b, &n| b.iter(|| rand_thread_sample(n)),
     );
     group.finish();
 }
