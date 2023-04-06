1878d86b16d15f6d28ab2f6e8d8a080277f39054
imrn99 <95699343+imrn99@users.noreply.github.com>
2023-04-06 12:00:13 +0200

    removed dead code: mc_particle_buffer

--- src/particles/mod.rs
+++ src/particles/mod.rs
@@ -1,7 +1,6 @@
 pub mod load_particle;
 pub mod mc_base_particle;
 pub mod mc_particle;
-pub mod mc_particle_buffer;
 pub mod particle_container;
 pub mod particle_vault;
 pub mod particle_vault_container;
