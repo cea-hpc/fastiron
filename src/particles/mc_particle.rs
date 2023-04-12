b09718f2a15a2a2d561d87052f31041850eb47a5
imrn99 <95699343+imrn99@users.noreply.github.com>
2023-04-12 15:57:05 +0200

    removed unused Display impl for MCParticle

--- src/particles/mc_particle.rs
+++ src/particles/mc_particle.rs
@@ -3,8 +3,6 @@
 //! This module contains code of an extended particle structure used
 //! for computations.
 
-use std::fmt::Display;
-
 use num::zero;
 
 use crate::{
@@ -70,57 +68,6 @@ impl<T: CustomFloat> MCParticle<T> {
     /// direction and distance
     pub fn move_particle(&mut self, distance: T) {
         self.base_particle.coordinate += self.direction_cosine.dir * distance;
-    }
-}
-
-// replaces original method `PrintParticle`
-impl<T: CustomFloat> Display for MCParticle<T> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        writeln!(
-            f,
-            "coordinate: {} {} {}",
-            self.base_particle.coordinate.x,
-            self.base_particle.coordinate.y,
-            self.base_particle.coordinate.z
-        )?;
-        writeln!(
-            f,
-            "velocity: {} {} {}",
-            self.base_particle.velocity.x,
-            self.base_particle.velocity.y,
-            self.base_particle.velocity.z
-        )?;
-        writeln!(
-            f,
-            "direction cosine: {} {} {}",
-            self.direction_cosine.dir.x, self.direction_cosine.dir.y, self.direction_cosine.dir.z
-        )?;
-        writeln!(f, "kinetic energy: {}", self.base_particle.kinetic_energy)?;
-        writeln!(f, "weight: {}", self.base_particle.weight)?;
-        writeln!(f, "time to census: {}", self.base_particle.time_to_census)?;
-        writeln!(f, "total cross section: {}", self.total_cross_section)?;
-        writeln!(f, "age: {}", self.base_particle.age)?;
-        writeln!(
-            f,
-            "num mean free paths: {}",
-            self.base_particle.num_mean_free_paths
-        )?;
-        writeln!(f, "mean free path: {}", self.mean_free_path)?;
-        writeln!(f, "segment path length: {}", self.segment_path_length)?;
-        writeln!(
-            f,
-            "random number seed: {}",
-            self.base_particle.random_number_seed
-        )?;
-        writeln!(f, "identifier: {}", self.base_particle.identifier)?;
-        writeln!(f, "last event: {:?}", self.base_particle.last_event)?;
-        writeln!(f, "num segments: {}", self.base_particle.num_segments)?;
-        writeln!(f, "species: {:?}", self.base_particle.species)?;
-        writeln!(f, "energy group: {}", self.energy_group)?;
-        writeln!(f, "domain: {}", self.base_particle.domain)?;
-        writeln!(f, "cell: {}", self.base_particle.cell)?;
-        writeln!(f, "facet: {}", self.facet)?;
-        writeln!(f, "normal dot: {}", self.normal_dot)
     }
 }
 
