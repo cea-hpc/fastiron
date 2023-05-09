a02832a6644ed0bf3b67ffab2a29346b74458433
imrn99 <95699343+imrn99@users.noreply.github.com>
2023-05-09 11:45:48 +0200

    stats: structures doc

--- fastiron-stats/src/structures.rs
+++ fastiron-stats/src/structures.rs
@@ -1,7 +1,10 @@
 use std::{iter::zip, ops::Index};
 
+/// Number of fields presented in the tallies report produced
+/// by `fastiron`.
 pub const N_TALLIED_DATA: usize = 17;
 
+/// Enum used to represent & map tallied data and their indexes.
 #[derive(Debug)]
 pub enum TalliedData {
     Cycle = 0,
@@ -23,6 +26,7 @@ pub enum TalliedData {
     CycleSync = 16,
 }
 
+/// Enum used to represent & map timers breakdown and their indexes.
 #[derive(Debug, Clone, Copy)]
 pub enum TimerSV {
     Main = 0,
@@ -33,20 +37,30 @@ pub enum TimerSV {
     CycleSync = 5,
 }
 
+/// Enum used to represent the nature of the scaling study.
 #[derive(Debug, Clone, Copy)]
 pub enum ProgressionType {
     Arithmetic,
     Geometric,
 }
 
+/// Structure used to model finite discrete random variables.
+///
+/// This structure is not meant to be modified. It should be initialized with all
+/// values using the provided constructor.
 #[derive(Debug)]
 pub struct FiniteDiscreteRV {
+    /// Values taken by the random variables.
     pub values: Vec<f64>,
+    /// Associated mean.
     pub mean: f64,
+    /// Associated variance.
     pub variance: f64,
 }
 
 impl FiniteDiscreteRV {
+    /// Constructor. Takes a slice as values and computes key associated values
+    /// before returning the object.
     pub fn new(values: &[f64]) -> Self {
         let n_val = values.len() as f64;
         let val = values.to_vec();
@@ -62,11 +76,13 @@ impl FiniteDiscreteRV {
         }
     }
 
+    /// Returns the number of values taken by the (discrete, finite) random variable
     pub fn n_val(&self) -> usize {
         self.values.len()
     }
 }
 
+/// Returns the covariance of two given [FiniteDiscreteRV].
 pub fn covariance(x: &FiniteDiscreteRV, y: &FiniteDiscreteRV) -> f64 {
     assert_eq!(x.n_val(), y.n_val());
     let iter = zip(x.values.iter(), y.values.iter());
@@ -75,25 +91,36 @@ pub fn covariance(x: &FiniteDiscreteRV, y: &FiniteDiscreteRV) -> f64 {
     cov
 }
 
+/// Returns the correlation coefficient of two given [FiniteDiscreteRV].
+///
+/// The function checks if `x` and `y` have non-zero variance. If this is the case,
+/// 0 is returned. It means variables are independent. While this may be technically
+/// false, it allows for generic computations
 pub fn correlation(x: &FiniteDiscreteRV, y: &FiniteDiscreteRV) -> f64 {
     if (x.variance == 0.0) | (y.variance == 0.0) {
-        // 0 means variables are independent
-        // this may be technically false but it allows for generic computations
+        //
         return 0.0;
     }
     let cov = covariance(x, y);
     cov / (x.variance * y.variance).sqrt()
 }
 
+/// Structure used to represent the summarized data of the timers.
 #[derive(Default, Clone, Copy, Debug)]
 pub struct SummarizedVariable {
+    /// Average value taken by the timer.
     pub mean: f64,
+    /// Lowest value taken by the timer.
     pub lowest: f64,
+    /// Highest value taken by the timer.
     pub highest: f64,
+    /// Sum of all value taken by the timer.
     pub total: f64,
 }
 
+/// Structure used to reprensent the entire timer report of a single simulation
 pub struct TimerReport {
+    /// Array of the section timers.
     pub timers_data: [SummarizedVariable; 6],
 }
 
