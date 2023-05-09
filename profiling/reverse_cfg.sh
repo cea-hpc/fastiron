0c9107b46b916b889306e30b33dd172d7633e28d
imrn99 <95699343+imrn99@users.noreply.github.com>
2023-05-09 10:42:43 +0200

    new script & input file for stats tool

---
+++ profiling/reverse_cfg.sh
@@ -0,0 +1,12 @@
+# This script used to reverse the change made by the bench_cfg.sh script
+# The governor option might change according o the machine
+
+# To be run as root!
+echo "Enabling turbo boost"
+echo 0 > /sys/devices/system/cpu/intel_pstate/no_turbo
+
+echo "Using performance governor"
+for i in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+do
+  echo ondemand > $i
+done
