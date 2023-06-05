21b5a660feb27ed8c86070fdd4eb64a26bed3a4c
imrn99 <95699343+imrn99@users.noreply.github.com>
2023-06-05 15:56:21 +0200

    perf script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,4 @@
 
 
-fastiron -i ./input_files/profiling/short_CTS2.inp -c -n 40960 -r 40
+fastiron -i ./input_files/profiling/short_CTS2.inp -c -n 40960 -r 0 # test the default value of rayon
+perf record fastiron -i ./input_files/profiling/one_cycle_CTS2.inp -n 4096000 -r 40
