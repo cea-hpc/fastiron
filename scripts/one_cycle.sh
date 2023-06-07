8aa7aef855f78f2936d5febb3b7a4813b4dcf2bc
imrn99 <95699343+imrn99@users.noreply.github.com>
2023-06-07 14:14:04 +0200

    simple perf script

---
+++ scripts/one_cycle.sh
@@ -0,0 +1,4 @@
+PERF = perf_5.10
+
+$PERF record fastiron -i ./input_files/profiling/OCCTS2.inp -n 4096000 -r 40 -c
+$PERF stat -d fastiron -i ./input_files/profiling/OCCTS2.inp -n 4096000 -r 40 -c
--- scripts/test.sh
+++
@@ -1,4 +0,0 @@
-
-
-fastiron -i ./input_files/profiling/short_CTS2.inp -c -n 40960 -r 0 # test the default value of rayon
-perf record fastiron -i ./input_files/profiling/one_cycle_CTS2.inp -n 4096000 -r 40
