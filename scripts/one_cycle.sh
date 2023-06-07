25d0cb7772704096557530a8346ba209382923b9
imrn99 <95699343+imrn99@users.noreply.github.com>
2023-06-07 15:06:28 +0200

    changed script/input to take advantage of CLI

--- scripts/one_cycle.sh
+++ scripts/one_cycle.sh
@@ -1,4 +1,14 @@
 PERF=perf_5.10 # until container is fixed 
 
-$PERF record fastiron -i ./input_files/profiling/OCCTS2.inp -n 4096000 -r 40 -c
-$PERF stat -d fastiron -i ./input_files/profiling/OCCTS2.inp -n 4096000 -r 40 -c
+$PERF record fastiron \
+    -i ./input_files/profiling/OCCTS2.inp \
+    -N 1 \
+    -n 4096000 \
+    -r 40 \
+    -c
+$PERF stat -d fastiron \
+    -i ./input_files/profiling/OCCTS2.inp \
+    -N 1 \
+    -n 4096000 \
+    -r 40 \
+    -c
