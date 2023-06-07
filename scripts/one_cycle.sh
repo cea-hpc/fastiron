5ebe3caf9fc709afa888386a0e43e6ac873cccbc
imrn99 <95699343+imrn99@users.noreply.github.com>
2023-06-07 14:42:12 +0200

    fix script

--- scripts/one_cycle.sh
+++ scripts/one_cycle.sh
@@ -1,4 +1,4 @@
-PERF = perf_5.10
+PERF=perf_5.10 # until container is fixed 
 
 $PERF record fastiron -i ./input_files/profiling/OCCTS2.inp -n 4096000 -r 40 -c
 $PERF stat -d fastiron -i ./input_files/profiling/OCCTS2.inp -n 4096000 -r 40 -c
