18e5629a9bc2c7e7e63c5f0198ef5c70f19ccb8c
imrn99 <95699343+imrn99@users.noreply.github.com>
2023-06-08 09:06:03 +0200

    added custom n_particles support for strong scaling + chmod+x

--- scripts/scaling/CTS2_strong.sh
+++ scripts/scaling/CTS2_strong.sh
@@ -3,14 +3,23 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # Only the number of threads scales
+# Target number of particles can be specified but a default value is provided
+
+N_PARTICLES=81920
+
+if [ $# -eq 1 ]
+  then
+    N_PARTICLES=$1
+fi
 
 # target folder
-mkdir CTS2_strong_scaling
+TARGET_FOLDER=CTS2_strong_scaling/$N_PARTICLES
+mkdir $TARGET_FOLDER
 
-# 81920 particles -- 32*16*16 mesh -- 2 thread 
+# 81920 particles -- 32*16*16 mesh -- 1 thread 
 fastiron \
     -i ./input_files/profiling/CTS2.inp \
-    -n 81920 \
+    -n $N_PARTICLES \
     -X 32 \
     -Y 16 \
     -Z 16 \
@@ -20,5 +29,5 @@ fastiron \
     -r 1 \
     -c \
 
-mv tallies_report.csv CTS2_strong_scaling/tallies_r1.csv
-mv timers_report.csv CTS2_strong_scaling/timers_r1.csv
+mv tallies_report.csv $TARGET_FOLDER/tallies_r1.csv
+mv timers_report.csv $TARGET_FOLDER/timers_r1.csv
