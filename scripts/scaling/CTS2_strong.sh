888d87cd13961585294b376b8fad1f0ad9326ac2
imrn99 <95699343+imrn99@users.noreply.github.com>
2023-06-08 09:11:02 +0200

    CTS2 strong scaling script complete

--- scripts/scaling/CTS2_strong.sh
+++ scripts/scaling/CTS2_strong.sh
@@ -31,3 +31,99 @@ fastiron \
 
 mv tallies_report.csv $TARGET_FOLDER/tallies_r1.csv
 mv timers_report.csv $TARGET_FOLDER/timers_r1.csv
+
+# 32*16*16 mesh -- 2 thread 
+fastiron \
+    -i ./input_files/profiling/CTS2.inp \
+    -n $N_PARTICLES \
+    -X 32 \
+    -Y 16 \
+    -Z 16 \
+    -x 32 \
+    -y 16 \
+    -z 16 \
+    -r 2 \
+    -c \
+
+mv tallies_report.csv $TARGET_FOLDER/tallies_r2.csv
+mv timers_report.csv $TARGET_FOLDER/timers_r2.csv
+
+# 32*16*16 mesh -- 4 thread 
+fastiron \
+    -i ./input_files/profiling/CTS2.inp \
+    -n $N_PARTICLES \
+    -X 32 \
+    -Y 16 \
+    -Z 16 \
+    -x 32 \
+    -y 16 \
+    -z 16 \
+    -r 4 \
+    -c \
+
+mv tallies_report.csv $TARGET_FOLDER/tallies_r4.csv
+mv timers_report.csv $TARGET_FOLDER/timers_r4.csv
+
+# 32*16*16 mesh -- 8 thread 
+fastiron \
+    -i ./input_files/profiling/CTS2.inp \
+    -n $N_PARTICLES \
+    -X 32 \
+    -Y 16 \
+    -Z 16 \
+    -x 32 \
+    -y 16 \
+    -z 16 \
+    -r 8 \
+    -c \
+
+mv tallies_report.csv $TARGET_FOLDER/tallies_r8.csv
+mv timers_report.csv $TARGET_FOLDER/timers_r8.csv
+
+# 32*16*16 mesh -- 16 thread 
+fastiron \
+    -i ./input_files/profiling/CTS2.inp \
+    -n $N_PARTICLES \
+    -X 32 \
+    -Y 16 \
+    -Z 16 \
+    -x 32 \
+    -y 16 \
+    -z 16 \
+    -r 16 \
+    -c \
+
+mv tallies_report.csv $TARGET_FOLDER/tallies_r16.csv
+mv timers_report.csv $TARGET_FOLDER/timers_r16.csv
+
+# 32*16*16 mesh -- 32 thread 
+fastiron \
+    -i ./input_files/profiling/CTS2.inp \
+    -n $N_PARTICLES \
+    -X 32 \
+    -Y 16 \
+    -Z 16 \
+    -x 32 \
+    -y 16 \
+    -z 16 \
+    -r 32 \
+    -c \
+
+mv tallies_report.csv $TARGET_FOLDER/tallies_r32.csv
+mv timers_report.csv $TARGET_FOLDER/timers_r32.csv
+
+# 32*16*16 mesh -- 36 thread 
+fastiron \
+    -i ./input_files/profiling/CTS2.inp \
+    -n $N_PARTICLES \
+    -X 32 \
+    -Y 16 \
+    -Z 16 \
+    -x 32 \
+    -y 16 \
+    -z 16 \
+    -r 36 \
+    -c \
+
+mv tallies_report.csv $TARGET_FOLDER/tallies_r36.csv
+mv timers_report.csv $TARGET_FOLDER/timers_r36.csv
