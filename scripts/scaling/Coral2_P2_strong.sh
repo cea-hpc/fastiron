785ecd31c4936fbe120844bf9664d900903c6974
imrn99 <95699343+imrn99@users.noreply.github.com>
2023-06-08 13:59:46 +0200

    Coral2 P2 strong scaling script

--- scripts/scaling/Coral2_P2_strong.sh
+++ scripts/scaling/Coral2_P2_strong.sh
@@ -26,3 +26,73 @@ fastiron \
 
 mv tallies_report.csv $TARGET_FOLDER/tallies_r1.csv
 mv timers_report.csv $TARGET_FOLDER/timers_r1.csv
+
+# 22*22*22 mesh -- 2 threads 
+fastiron \
+    -i ./input_files/profiling/Coral2_P2_sized.inp \
+    -n $N_PARTICLES \
+    -r 2 \
+    -c \
+
+mv tallies_report.csv $TARGET_FOLDER/tallies_r2.csv
+mv timers_report.csv $TARGET_FOLDER/timers_r2.csv
+
+# 22*22*22 mesh -- 4 threads 
+fastiron \
+    -i ./input_files/profiling/Coral2_P2_sized.inp \
+    -n $N_PARTICLES \
+    -r 4 \
+    -c \
+
+mv tallies_report.csv $TARGET_FOLDER/tallies_r4.csv
+mv timers_report.csv $TARGET_FOLDER/timers_r4.csv
+
+# 22*22*22 mesh -- 8 threads 
+fastiron \
+    -i ./input_files/profiling/Coral2_P2_sized.inp \
+    -n $N_PARTICLES \
+    -r 8 \
+    -c \
+
+mv tallies_report.csv $TARGET_FOLDER/tallies_r8.csv
+mv timers_report.csv $TARGET_FOLDER/timers_r8.csv
+
+# 22*22*22 mesh -- 16 threads 
+fastiron \
+    -i ./input_files/profiling/Coral2_P2_sized.inp \
+    -n $N_PARTICLES \
+    -r 16 \
+    -c \
+
+mv tallies_report.csv $TARGET_FOLDER/tallies_r16.csv
+mv timers_report.csv $TARGET_FOLDER/timers_r16.csv
+
+# 22*22*22 mesh -- 32 threads 
+fastiron \
+    -i ./input_files/profiling/Coral2_P2_sized.inp \
+    -n $N_PARTICLES \
+    -r 32 \
+    -c \
+
+mv tallies_report.csv $TARGET_FOLDER/tallies_r32.csv
+mv timers_report.csv $TARGET_FOLDER/timers_r32.csv
+
+# 22*22*22 mesh -- 64 threads 
+fastiron \
+    -i ./input_files/profiling/Coral2_P2_sized.inp \
+    -n $N_PARTICLES \
+    -r 64 \
+    -c \
+
+mv tallies_report.csv $TARGET_FOLDER/tallies_r64.csv
+mv timers_report.csv $TARGET_FOLDER/timers_r64.csv
+
+# 22*22*22 mesh -- 128 threads 
+fastiron \
+    -i ./input_files/profiling/Coral2_P2_sized.inp \
+    -n $N_PARTICLES \
+    -r 128 \
+    -c \
+
+mv tallies_report.csv $TARGET_FOLDER/tallies_r128.csv
+mv timers_report.csv $TARGET_FOLDER/timers_r128.csv
