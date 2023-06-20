0eb28fdda88c13237583299d9dbb38a733462b84
imrn99 <95699343+imrn99@users.noreply.github.com>
2023-06-20 10:43:09 +0200

    fi-stats: added scaling params to cli

--- fastiron-stats/src/io/command_line.rs
+++ fastiron-stats/src/io/command_line.rs
@@ -5,18 +5,44 @@ use clap::Parser;
 #[command(author, version, about, arg_required_else_help(true))]
 pub struct Cli {
     /// name of the two timers report files to compare - old report first, new report second
-    #[arg(short = 't', long = "timers-comparison", num_args(2))]
+    #[arg(short = 'T', long = "timers-comparison", num_args(2))]
     pub comparison: Option<Vec<String>>,
 
     /// name of the tallies file to analyze
-    #[arg(short = 'e', long = "event-correlation", num_args(1))]
+    #[arg(short = 'E', long = "event-correlation", num_args(1))]
     pub correlation: Option<String>,
 
     /// root path of timers file for weak scaling data
-    #[arg(short = 'W', long = "weak-scaling", num_args(1))]
+    #[arg(
+        short = 'W',
+        long = "weak-scaling",
+        num_args(1),
+        requires("n-start-thread"),
+        requires("n-iter-thread"),
+        requires("factor-thread")
+    )]
     pub weak_scaling_root: Option<String>,
 
     /// root path of timers file for strong scaling data
-    #[arg(short = 'S', long = "strong-scaling", num_args(1))]
+    #[arg(
+        short = 'S',
+        long = "strong-scaling",
+        num_args(1),
+        requires("n-start-thread"),
+        requires("n-iter-thread"),
+        requires("factor-thread")
+    )]
     pub strong_scaling_root: Option<String>,
+
+    /// starting number of threads for scaling data
+    #[arg(short = 't', long = "n-start-thread", num_args(1))]
+    pub t_init: Option<usize>,
+
+    /// starting number of threads for scaling data
+    #[arg(short = 'i', long = "n-iter-thread", num_args(1))]
+    pub t_iter: Option<usize>,
+
+    /// starting number of threads for scaling data
+    #[arg(short = 'f', long = "factor-thread", num_args(1))]
+    pub t_factor: Option<usize>,
 }
