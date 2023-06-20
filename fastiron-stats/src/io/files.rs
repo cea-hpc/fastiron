8fbc4bca8de91f6583b432caf6c3471d33ce1fe8
imrn99 <95699343+imrn99@users.noreply.github.com>
2023-06-20 15:42:41 +0200

    fi-stats:removed old correlation code

--- fastiron-stats/src/io/files.rs
+++ fastiron-stats/src/io/files.rs
@@ -8,37 +8,7 @@ use std::{
     io::Write,
 };
 
-use crate::structures::raw::{TalliedVariable, TimerReport, TimerSV, N_TALLIED_DATA};
-
-// =======
-// Reading
-
-/// Builds an organized structure from a tallies report saved by the
-/// `fastiron` binary.
-///
-/// This function is a general reading function,
-/// not all the data will necessarly be used.
-///
-/// Any change done to the tallied data / its representation will
-/// demand an update of this function.
-pub fn read_tallies(file_name: &str) -> [TalliedVariable; N_TALLIED_DATA] {
-    let file = File::open(file_name).unwrap();
-    let mut reader = csv::ReaderBuilder::new().delimiter(b';').from_reader(file);
-    let mut values: [Vec<f64>; N_TALLIED_DATA] = Default::default();
-    values.iter_mut().for_each(|v| v.reserve(100));
-    // for each line
-    for result in reader.records() {
-        let mut record = result.unwrap();
-        record.trim();
-        // for each column
-        (0..N_TALLIED_DATA).for_each(|idx| {
-            let val = record.get(idx).unwrap();
-            values[idx].push(val.parse().unwrap())
-        })
-    }
-    // convert value vectors to our structure
-    values.map(|val| TalliedVariable::new(&val))
-}
+use crate::structures::raw::{TimerReport, TimerSV};
 
 /// Builds an organized structure from a (formatted) list of timers reports.
 ///
@@ -57,72 +27,6 @@ pub fn get_scaling_data(
             (TimerReport::from(File::open(filename).unwrap()), n_thread)
         })
         .collect()
-}
-
-// =======
-// Writing
-
-/// Save the results of the correlation study.
-///
-/// The plotted matrix will have this structure:
-///
-/// \* | Absorb | Scatter | Fission | Collision | Census | NumSeg
-/// ---|--------|---------|---------|-----------|--------|--------
-/// CycleTracking | `c0` | `c1` | `c2` | `c3` | `c4` | `c5`
-///
-pub fn save_tracking_results(tracking_res: &[f64]) {
-    let mut file = OpenOptions::new()
-        .write(true)
-        .create(true)
-        .truncate(true)
-        .open("tracking.dat")
-        .unwrap();
-    writeln!(file, ",Absorb,Scatter,Fission,Collision,Census,NumSeg").unwrap();
-    // write correlation coeffs
-    writeln!(
-        file,
-        "CycleTracking, {:.5}, {:.5}, {:.5}, {:.5}, {:.5}, {:.5}",
-        tracking_res[0],
-        tracking_res[1],
-        tracking_res[2],
-        tracking_res[3],
-        tracking_res[4],
-        tracking_res[5],
-    )
-    .unwrap();
-    // padding values for it to be considered a matrix
-    writeln!(file, "Dummy, 0, 0, 0, 0, 0, 0").unwrap();
-}
-
-/// Save the results of the correlation study.
-///
-/// The plotted matrix will have this structure:
-///
-/// \* | Rr | Split
-/// ---|----|-------
-/// PopulationControl | `c0` | `c1`
-/// CycleSync         | `c2` | `c3`
-///
-pub fn save_popsync_results(popsync_res: &[f64]) {
-    let mut file = OpenOptions::new()
-        .write(true)
-        .create(true)
-        .truncate(true)
-        .open("popsync.dat")
-        .unwrap();
-    writeln!(file, ",Rr,Split").unwrap();
-    writeln!(
-        file,
-        "CycleSync, {:.5}, {:.5}",
-        popsync_res[1], popsync_res[2]
-    )
-    .unwrap();
-    writeln!(
-        file,
-        "PopulationControl, {:.5}, {:.5}",
-        popsync_res[4], popsync_res[5]
-    )
-    .unwrap();
 }
 
 /// Save the results of the scaling study.
