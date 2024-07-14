eea8efc821a15ac2f2f0321439d22db3a2dd956d
imrn99 <95699343+imrn99@users.noreply.github.com>
2024-07-14 18:52:07 +0200

    add a CLI switch to bind threads
    when `--bind-threads` is present, rayon threads will be bound
    to cores

--- fastiron/src/main.rs
+++ fastiron/src/main.rs
@@ -70,7 +70,7 @@ fn main() {
     // rayon only => one global thread pool
     if mcdata.exec_info.exec_policy == ExecPolicy::Rayon {
         // custom thread-pool init in this case
-        if mcdata.exec_info.n_rayon_threads != 0 {
+        if mcdata.exec_info.n_rayon_threads != 0 && mcdata.exec_info.bind_threads {
             let topo = Arc::new(Mutex::new(Topology::new().unwrap()));
             ThreadPoolBuilder::new()
                 .num_threads(mcdata.exec_info.n_rayon_threads)
